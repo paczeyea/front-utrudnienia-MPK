b8e35fdd9b34d5dd439963e621a41382112afa8e
Emilian Maćkowski <paczeyea@gmail.com>
2024-04-28 21:07:55 +0200

    mapa jak w jakdojade, dodać przyciski

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  ngOnInit(): void {
+    const map = L.map('map').setView([51.1103, 17.0423], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+}
