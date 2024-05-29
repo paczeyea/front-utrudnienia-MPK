bb8c0c8fb6544cc59e189ff3105246c0418bac03
Emilian Maćkowski <paczeyea@gmail.com>
2024-05-29 12:47:41 +0200

    moze dziala

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker'; 
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })]
+        registrationStrategy: 'registerWhenStable:30000',
+    }), provideHttpClient()]
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { BrowserModule } from '@angular/platform-browser';
     CommonModule,
     HttpClientModule
 ],
+providers: [HttpClientModule],
 bootstrap: [AppComponent]
 
 })
