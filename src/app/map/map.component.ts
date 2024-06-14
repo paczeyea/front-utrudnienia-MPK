f7e1d8b598bc32b6b3f08c07ab9b2d4129d98d20
Emilian Maćkowski <paczeyea@gmail.com>
2024-06-14 09:19:39 +0200

    poprawka tailwind loc-dialog schedule-dialog

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -111,12 +111,12 @@ export class MapComponent implements OnInit {
         }
       });
 
-    this.addStopSign(51.1356, 17.0376);
-    this.StopsService.getStopsFromfile().subscribe((stops: Stop[]) => {
-      for (const stop of stops) {
-        this.addStopSign(stop.stop_lat, stop.stop_lon);
-      }
-    });
+    // this.addStopSign(51.1356, 17.0376);
+    // this.StopsService.getStopsFromfile().subscribe((stops: Stop[]) => {
+    //   for (const stop of stops) {
+    //     this.addStopSign(stop.stop_lat, stop.stop_lon);
+    //   }
+    // });
 
 
 
--- src/app/modals/report-dialog/report-dialog.component.ts
+++ src/app/modals/report-dialog/report-dialog.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-report-dialog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './report-dialog.component.html',
   // styleUrl: './report-dialog.component.css'
 })
 export class ReportDialogComponent {
+  isOpen = false;
 
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
 }
